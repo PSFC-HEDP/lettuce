f8542c4bc57a1f0304d932de501cc3bbeaf58458
Justin Kunimune <justinkunimune@gmail.com>
2023-10-16 14:04:39 -0400

    fix squeue time format

--- check_on_lilac_runs.sh
+++ check_on_lilac_runs.sh
@@ -3,7 +3,7 @@
 module purge
 module load slurm/current
 
-export SLURM_DATE_FORMAT=relative
+export SLURM_TIME_FORMAT=relative
 echo "Code  Name   Slurm_ID     State      Reason     Time     Start       End"
 echo "---------- ---------- --------- ----------- -------- --------- ---------"
 squeue --user="$(whoami)" --states="all" --format="%12j %.8A %.9T %.11r %.8M %.9S %.9e" | grep "LILAC"
