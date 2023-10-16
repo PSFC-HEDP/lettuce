64944763385f5e9aaa231acd93ac59886adddfc5
Justin Kunimune <justinkunimune@gmail.com>
2023-10-16 15:01:01 -0400

    change squeue sort order

--- check_on_lilac_runs.sh
+++ check_on_lilac_runs.sh
@@ -6,4 +6,4 @@ module load slurm/current
 export SLURM_TIME_FORMAT=relative
 echo "Code  Name   Slurm_ID     State      Reason     Time     Start       End"
 echo "---------- ---------- --------- ----------- -------- --------- ---------"
-squeue --user="$(whoami)" --states="all" --format="%12j %.8A %.9T %.11r %.8M %.9S %.9e" | grep "LILAC"
+squeue --user="$(whoami)" --states="all" --sort="i" --format="%12j %.8A %.9T %.11r %.8M %.9S %.9e" | grep "LILAC"
