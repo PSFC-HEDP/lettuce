b145159a845db692012caf9822ce521312830f48
Justin Kunimune <justinkunimune@gmail.com>
2023-10-24 13:08:41 -0400

    correct some filenames

--- check_on_runs.sh
+++ check_on_runs.sh
@@ -4,6 +4,8 @@ module purge
 module load slurm/current
 
 export SLURM_TIME_FORMAT=relative
+queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%14j %.8A %.9T %.11r %.8M %.9S %.9e')"
 echo "Code  Name     Slurm_ID     State      Reason     Time     Start       End"
 echo "-------------- -------- --------- ----------- -------- --------- ---------"
-squeue --user="$(whoami)" --states="all" --sort="i" --format="%14j %.8A %.9T %.11r %.8M %.9S %.9e" | grep "LILAC"
+grep LILAC <<< "$queue"
+grep IRIS <<< "$queue"
