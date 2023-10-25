8be448f4dc50d7725e72d28734597d73f1f978f0
Justin Kunimune <justinkunimune@gmail.com>
2023-10-25 14:18:37 -0400

    more intelligent run checking
    it's always botherd me how squeue returns just a header when there are no runs.  I think it's nicer if it actually says "no runs".  so that's what this is.  it was a little tricky just because of how grep doesn't put a newline on the end of its output because echo always puts a newline at the end of *its* output... but it works.

--- check_on_runs.sh
+++ check_on_runs.sh
@@ -5,7 +5,17 @@ module load slurm/current
 
 export SLURM_TIME_FORMAT=relative
 queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%14j %.8A %.9T %.11r %.8M %.9S %.9e')"
-echo "Code  Name     Slurm_ID     State      Reason     Time     Start       End"
-echo "-------------- -------- --------- ----------- -------- --------- ---------"
-grep LILAC <<< "$queue"
-grep IRIS <<< "$queue"
+lilac_runs="$(grep LILAC <<< "$queue")"
+iris_runs="$(grep IRIS <<< "$queue")"
+if [ "$lilac_runs" != "" ] || [ "$iris_runs" != "" ]; then
+	echo "Code  Name     Slurm_ID     State      Reason     Time     Start       End"
+	echo "--------------+--------+---------+-----------+--------+---------+---------"
+	if [ "$lilac_runs" != "" ]; then
+		echo "$lilac_runs"
+	fi
+	if [ "$iris_runs" != "" ]; then
+		echo "$iris_runs"
+	fi
+else
+	echo "No active or recent runs"
+fi
