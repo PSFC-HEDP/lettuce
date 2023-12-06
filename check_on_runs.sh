3678ada4105c91821cbe9c9fdb23524d6ba9b561
Justin Kunimune <justinkunimune@gmail.com>
2023-12-06 10:54:24 -0500

    fix format string
    this seems to work in some terminals but not others...

--- check_on_runs.sh
+++ check_on_runs.sh
@@ -4,7 +4,7 @@ module purge
 module load slurm/current
 
 export SLURM_TIME_FORMAT=relative
-queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%14j %.8A %.9T %.11r %.8M %.9S %.9e')"
+queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%.14j %.8A %.9T %.11r %.8M %.9S %.9e')"
 lilac_runs="$(grep LILAC <<< "$queue")"
 iris_runs="$(grep IRIS <<< "$queue")"
 if [ "$lilac_runs" != "" ] || [ "$iris_runs" != "" ]; then
