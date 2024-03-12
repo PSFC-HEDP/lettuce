c5ddd71cc007fe18d13122a6e9a81b1366beed0b
Justin Kunimune <justinkunimune@gmail.com>
2024-03-12 12:17:17 -0700

    make space for more squeue info
    I think most users can accomodate a few more characters in their run names.

--- check_on_runs.sh
+++ check_on_runs.sh
@@ -4,12 +4,12 @@ module purge
 module load slurm/current
 
 export SLURM_TIME_FORMAT=relative
-queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%.14j %.8A %.9T %.11r %.8M %.9S %.9e')"
+queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%.17j %.8A %.9T %.11r %.8M %.10S %.10e')"
 lilac_runs="$(grep LILAC <<< "$queue")"
 iris_runs="$(grep IRIS <<< "$queue")"
 if [ "$lilac_runs" != "" ] || [ "$iris_runs" != "" ]; then
 	echo "Code  Name     Slurm_ID     State      Reason     Time     Start       End"
-	echo "--------------+--------+---------+-----------+--------+---------+---------"
+	echo "-----------------+--------+---------+-----------+--------+----------+----------"
 	if [ "$lilac_runs" != "" ]; then
 		echo "$lilac_runs"
 	fi
