47e7e725f0dde148ab2570c60aaf3dde5a7e69e2
Justin Kunimune <jkunimun@bluehive.bluehive.circ.private>
2024-05-09 10:40:08 -0400

    fix spacing in check_on_runs.sh

--- check_on_runs.sh
+++ check_on_runs.sh
@@ -8,7 +8,7 @@ queue="$(squeue --user="$(whoami)" --states=all --sort=i --format='%.17j %.8A %.
 lilac_runs="$(grep LILAC <<< "$queue")"
 iris_runs="$(grep IRIS <<< "$queue")"
 if [ "$lilac_runs" != "" ] || [ "$iris_runs" != "" ]; then
-	echo "Code  Name     Slurm_ID     State      Reason     Time     Start       End"
+	echo " Code        Name Slurm ID     State      Reason     Time      Start        End"
 	echo "-----------------+--------+---------+-----------+--------+----------+----------"
 	if [ "$lilac_runs" != "" ]; then
 		echo "$lilac_runs"
